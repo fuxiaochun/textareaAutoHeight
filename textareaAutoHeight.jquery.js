c8e81c9bfe850c3a9abde36bd656b99873dd72ba
xiaochun <cqouting@sina.com>
2016-08-01 11:51:36 +0800

    优化版

--- textareaAutoHeight.jquery.js
+++ textareaAutoHeight.jquery.js
@@ -5,40 +5,51 @@
  */
 (function($) {
 	$.fn.tah = function(options) {
+		var div = null;
+		var styles = null;
 		var config = {
 			maxHeight: 200
 		};
+
 		if (!this.length) {
 			return false;
 		}
+
 		$.extend(config, (options || {}));
+		div = $('<div contenteditable="true"></div>');
+		styles = 'width,fontSize,fontWeight,fontStyle,fontFamily,lineHeight,padding,wordWrap,wordBreak,whiteSpace,letterSpacing'.split(',');
+		div.css({
+			left: -(9e5),
+			position: 'absolute',
+			zIndex: '-1'
+		});
+		div.appendTo($('body'));
+
 		this.each(function(i, t) {
-			var divP = $('<div><div contenteditable="true"></div></div>');
-			var div = divP.children('div');
-			var styles = 'width,fontSize,fontWeight,fontStyle,fontFamily,lineHeight,padding,wordWrap,wordBreak,whiteSpace,letterSpacing'.split(',');
-			divP.css({
-				height: 0,
-				overflow: 'hidden',
-				position: 'relative',
-				zIndex: '-1'
-			});
 			t = $(t).css({
 				resize: 'none'
 			});
-			t.after(divP);
-			$.each(styles, function(i, p) {
-				div.css(p, t.css(p));
+			t.on('focus', function() {
+				$.each(styles, function(i, p) {
+					div.css(p, t.css(p));
+				});
 			});
-			t.on('blur focus input change propertychange keydown', function() {
+			t.on('focus blur input propertychange', function() {
 				var val = t.val().replace(/\r?\n/g, '<br/>&#8203;');
 				div.html(val);
 				if (div.height() > config.maxHeight) {
-					t.height(config.maxHeight);
-					t.scrollTop(div.innerHeight());
+					t.css({
+						height: config.maxHeight,
+						overflowY: 'scroll'
+					});
 					return;
 				}
+
 				if (div.height()) {
-					t.height(div.height());
+					t.css({
+						height: div.height(),
+						overflowY: 'hidden'
+					});
 				}
 			});
 		});
